feeb56e9b3b2e636129c7abd6726c4e75abc56d0
2wiktor2 <wiktor2011@gmail.com>
2020-04-09 11:08:58 +0300

    first commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String WEATHER_URL = "https://samples.openweathermap.org/data/2.5/weather?q=%s&appid=f42000c97a78ddd4b5f32081010bcd22&lang=ru&units=metric";
+    private EditText editTextCity;
+    private TextView textViewWeather;
+    private Button button;
+
+    // f42000c97a78ddd4b5f32081010bcd22
+    //    //https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22
+
+    // &lang=ru
+    // &units=metric
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextCity = findViewById(R.id.editTextCity);
+        textViewWeather = findViewById(R.id.textViewWeather);
+        button = findViewById(R.id.buttonShowWeather);
+        button.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        String city = editTextCity.getText().toString().trim();
+        if (!city.isEmpty()){
+            DownloadWeatherTask task = new DownloadWeatherTask();
+            String url = String.format(WEATHER_URL, city);
+            task.execute(url);
+        }
+    }
+
+    private  class DownloadWeatherTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            StringBuilder result = new StringBuilder();
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null){
+                    result.append(line);
+                    reader.readLine();
+                }
+                return result.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null){
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String city = jsonObject.getString("name");
+                String temp = jsonObject.getJSONObject("main").getString("temp");
+                String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                String weather = String.format("%s\nТемпература: %s\nНа улице: %s", city, temp,description);
+                textViewWeather.setText(weather);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
